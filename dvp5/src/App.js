d7d47c20bb9fc7728d91e25a42f668d9556c0047
mdcowan <mdcowan@student.fullsail.edu>
2019-11-25 19:44:28 -0500

    added React app

---
+++ dvp5/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Insert content here
+      </header>
+    </div>
+  );
+}
+
+export default App;
