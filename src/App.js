907c3323b6d33cd3fe9eb35c3b10a601fb80bf0d
mdcowan <mdcowan@student.fullsail.edu>
2020-01-09 20:00:00 -0500

    Proof of Concept complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Gnews from './components/gnews/Gnews';
-import Comicvine from './components/comicvine/Comicvine';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Gnews></Gnews>
-        <Comicvine></Comicvine>
       </header>
     </div>
   );
